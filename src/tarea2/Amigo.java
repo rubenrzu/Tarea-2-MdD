36053bbaa444ddcd1eb6132f1f9c4bcc00cbeb41
Rubén <Rubn@192.168.1.76>
2020-10-10 15:45:53 -0500

    V2

--- src/tarea2/Amigo.java
+++ src/tarea2/Amigo.java
@@ -1,5 +1,6 @@
 //   Hola Abi!!
 //  Comentario 2
+// Comentario 3
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates

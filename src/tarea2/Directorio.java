233ab05182b9e323cfced2ab4bbc3bdd4086252c
ruben <ruben@LenovoG>
2020-10-10 17:11:07 -0500

    Versión 1

--- src/tarea2/Directorio.java
+++ src/tarea2/Directorio.java
@@ -5,11 +5,29 @@
  */
 package tarea2;
 
+import static java.lang.System.in;
+import java.util.Scanner;
+
 /**
  *
- *
+ * @author ruben
  */
 public class Directorio {
+    private Contacto contacto[];
+    private int na;
+    private Scanner lector;
+    private Scanner lector1;
+    
+    public Directorio(int a){
+        contacto = new Contacto[(a>0) ? a:20];
+        na=0;
+        lector = new Scanner(in);
+        lector1 = new Scanner(in).useDelimiter("\n");
+    }
+    
+    public Directorio(){
+    this(20);
+    }
     
     
 }
